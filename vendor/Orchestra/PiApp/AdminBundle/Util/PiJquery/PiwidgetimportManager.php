16cd905a85bd5559eef0478cb14a95b3f75882a4
alhassane <etienne_delongeaux@hotmail.com>
2013-09-10 18:13:12 +0200

    - Modify PiwidgetimportManager and add default Action - Add dashboard in home after connexion

--- vendor/Orchestra/PiApp/AdminBundle/Util/PiJquery/PiwidgetimportManager.php
+++ vendor/Orchestra/PiApp/AdminBundle/Util/PiJquery/PiwidgetimportManager.php
@@ -31,13 +31,13 @@ class PiwidgetimportManager extends PiJqueryExtension
      * @var array
      * @static
      */
-    static $menus = array('widgetimport');
+    static $menus = array('default', 'widgetimport');
     
     /**
      * @var array
      * @static
     */
-    static $actions = array('widgetimport');
+    static $actions = array('default','widgetimport');
         
     /**
      * Constructor.
@@ -60,8 +60,13 @@ class PiwidgetimportManager extends PiJqueryExtension
     protected function init($options = null)
     {
         //css
-        $this->container->get('pi_app_admin.twig.extension.layouthead')->addCssFile("bundles/piappadmin/js/iGoogle/inettuts.js.css");
-        $this->container->get('pi_app_admin.twig.extension.layouthead')->addCssFile("bundles/piappadmin/js/iGoogle/inettuts.css");
+        if ( empty($options) || ($options == 'widgetimport') ) {
+            $this->container->get('pi_app_admin.twig.extension.layouthead')->addCssFile("bundles/piappadmin/js/iGoogle/css/widgetimport/inettuts.js.css");
+            $this->container->get('pi_app_admin.twig.extension.layouthead')->addCssFile("bundles/piappadmin/js/iGoogle/css/widgetimport/inettuts.css");
+        } elseif ($options == 'default') {
+            $this->container->get('pi_app_admin.twig.extension.layouthead')->addCssFile("bundles/piappadmin/js/iGoogle/css/default/inettuts.js.css");
+            $this->container->get('pi_app_admin.twig.extension.layouthead')->addCssFile("bundles/piappadmin/js/iGoogle/css/default/inettuts.css");
+        }
     }    
     
     /**
@@ -76,18 +81,21 @@ class PiwidgetimportManager extends PiJqueryExtension
     protected function render($options = null)
     {
         // Options management
-        if (!isset($options['action']) || empty($options['action']) || (isset($options['action']) && !in_array(strtolower($options['action']), self::$actions)) )
+        if (!isset($options['action']) || empty($options['action']) || (isset($options['action']) && !in_array(strtolower($options['action']), self::$actions)) ) {
             throw ExtensionException::optionValueNotSpecified('action', __CLASS__);
-        if (!isset($options['menu']) || empty($options['menu']) || (isset($options['menu']) && !in_array(strtolower($options['menu']), self::$menus)) )
+        }
+        if (!isset($options['menu']) || empty($options['menu']) || (isset($options['menu']) && !in_array(strtolower($options['menu']), self::$menus)) ) {
             throw ExtensionException::optionValueNotSpecified('menu', __CLASS__);
-        
+        }
+        // we set method names
         $method = strtolower($options['menu']) . "Menu";
         $action = strtolower($options['action']) . "Action";
-        
-        if (method_exists($this, $method))
+        // we set result
+        if (method_exists($this, $method)) {
             $result = $this->$method($options);
-        else
+        } else {
             throw ExtensionException::MethodUnDefined($method);
+        }
         
         return $this->$action($result, $options);
     }
@@ -103,17 +111,248 @@ class PiwidgetimportManager extends PiJqueryExtension
      * @author (c) Etienne de Longeaux <etienne_delongeaux@hotmail.com>
      * @author (c) Pedro Felix <contactpfelix@gmail.com>
      */
+    private function defaultAction($result, $options = null)
+    {
+        // set Options
+        if ( isset($options['locale']) ) {
+            $this->locale    = $options['locale'];
+        }
+        if ( !isset($options['template']) || empty($options['template']) ) {
+            throw ExtensionException::optionValueNotSpecified('template', __CLASS__);
+        } else {
+            $template    = $options['template'];
+        }
+    	// We open the buffer.
+    	ob_start ();
+    	?>
+                    <script type="text/javascript">
+                    //<![CDATA[
+                    
+                    var PIimportwidget = {
+                        
+                        jQuery : $,
+                        
+                        settings : {
+                            columns : '.column',
+                            widgetSelector: '.widget',
+                            handleSelector: '.widget-head',
+                            contentSelector: '.widget-content',
+                            widgetDefault : {
+                                movable: true,
+                                removable: true,
+                                collapsible: true,
+                                editable: true,
+                                colorClasses : ['color-yellow', 'color-red', 'color-blue', 'color-white', 'color-orange', 'color-green']
+                            },
+                            widgetIndividual : {
+                                intro : {
+                                    movable: false,
+                                    removable: false,
+                                    collapsible: false,
+                                    editable: false
+                                }
+                            }
+                        },
+        
+                        init : function () {
+                            this.addWidgetControls();
+                            this.makeSortable();
+                        },
+                        
+                        getWidgetSettings : function (id) {
+                            var $ = this.jQuery,
+                                settings = this.settings;
+                            return (id&&settings.widgetIndividual[id]) ? $.extend({},settings.widgetDefault,settings.widgetIndividual[id]) : settings.widgetDefault;
+                        },
+                        
+                        addWidgetControls : function () {
+                            var PIimportwidget = this,
+                                $ = this.jQuery,
+                                settings = this.settings;
+                                
+                            $(settings.widgetSelector, $(settings.columns)).each(function () {
+                                var thisWidgetSettings = PIimportwidget.getWidgetSettings(this.id);
+                                if (thisWidgetSettings.removable) {
+                                    $('<a href="#" class="remove">CLOSE</a>').mousedown(function (e) {
+                                        e.stopPropagation();    
+                                    }).click(function () {
+                                        if (confirm('This widget will be removed, ok?')) {
+                                            $(this).parents(settings.widgetSelector).animate({
+                                                opacity: 0    
+                                            },function () {
+                                                $(this).wrap('<div/>').parent().slideUp(function () {
+                                                    $(this).remove();
+                                                });
+                                            });
+                                        }
+                                        return false;
+                                    }).appendTo($(settings.handleSelector, this));
+                                }
+                                
+                                if (thisWidgetSettings.editable) {
+                                    $('<a href="#" class="edit">EDIT</a>').mousedown(function (e) {
+                                        e.stopPropagation();    
+                                    }).toggle(function () {
+                                        $(this).css({backgroundPosition: '-66px 0', width: '55px'})
+                                            .parents(settings.widgetSelector)
+                                                .find('.edit-box').show().find('input').focus();
+                                        return false;
+                                    },function () {
+                                        $(this).css({backgroundPosition: '', width: ''})
+                                            .parents(settings.widgetSelector)
+                                                .find('.edit-box').hide();
+                                        return false;
+                                    }).appendTo($(settings.handleSelector,this));
+                                }
+                                
+                                if (thisWidgetSettings.collapsible) {
+                                    $('<a href="#" class="collapse">COLLAPSE</a>').mousedown(function (e) {
+                                        e.stopPropagation();    
+                                    }).toggle(function () {
+                                        $(this).css({backgroundPosition: '-38px 0'})
+                                            .parents(settings.widgetSelector)
+                                                .find(settings.contentSelector).hide();
+                                        return false;
+                                    },function () {
+                                        $(this).css({backgroundPosition: ''})
+                                            .parents(settings.widgetSelector)
+                                                .find(settings.contentSelector).show();
+                                        return false;
+                                    }).prependTo($(settings.handleSelector,this));
+                                }
+                            });
+                            
+                        },
+    
+                        makeSortable : function () {
+                            var PIimportwidget = this,
+                                $ = this.jQuery,
+                                settings = this.settings,
+                                $sortableItems = (function () {
+                                    var notSortable = '';
+                                    $(settings.widgetSelector,$(settings.columns)).each(function (i) {
+                                        if (!PIimportwidget.getWidgetSettings(this.id).movable) {
+                                            if (!this.id) {
+                                                this.id = 'widget-no-id-' + i;
+                                            }
+                                            //notSortable += '#' + this.id + ',';
+                                        }
+                                    });
+                                    return $('> li:not(' + notSortable + ')', settings.columns);
+                                })();
+                            
+                            $sortableItems.find(settings.handleSelector).css({
+                                cursor: 'move'
+                            }).mousedown(function (e) {
+                                $sortableItems.css({width:''});
+                                $(this).parent().css({
+                                    width: $(this).parent().width() + 'px'
+                                });
+                            }).mouseup(function () {
+                                if (!$(this).parent().hasClass('dragging')) {
+                                    $(this).parent().css({width:''});
+                                } else {
+                                    $(settings.columns).sortable('disable');
+                                }
+                            });
+        
+                            $(settings.columns).sortable({
+                                items: $sortableItems,
+                                connectWith: $(settings.columns),
+                                handle: settings.handleSelector,
+                                placeholder: 'widget-placeholder',
+                                forcePlaceholderSize: true,
+                                revert: 300,
+                                delay: 100,
+                                opacity: 0.8,
+                                containment: 'document',
+                                start: function (e,ui) {
+                                    $(ui.helper).addClass('dragging');
+                                },
+                                stop: function (e,ui) {
+                                    $(ui.item).css({width:''}).removeClass('dragging');
+                                    $(settings.columns).sortable('enable');
+                                }
+                            });
+                        }
+                      
+                    };
+        
+                    PIimportwidget.init();
+    
+                    //]]>
+                    </script>
+                        
+            <?php 
+            // We retrieve the contents of the buffer.
+            $_content = ob_get_contents ();
+            // We clean the buffer.
+            ob_clean ();
+            // We close the buffer.
+            ob_end_flush ();
+            
+            if ( !isset($options['noJs']) || !($options['noJs']) ) {
+                return    $result . $_content;        
+            } else {
+                return    $result;
+            }
+        }    
+    
+    /**
+     * Return the template content with iGoogle plugin.
+     *
+     * <code>
+     *        {% set options_widgetimport = {
+     *                'action':'default',
+     *                'menu': 'default',
+     *                'template': 'home.html.twig',
+     *                'locale': app.request.locale,
+     *                'noJs' : false,
+     *            }
+     *        %}
+     *        {{ renderJquery('TOOL', 'widgetimport', options_widgetimport )|raw }}
+     * </code>
+     *
+     * @param    array $options
+     * @access public
+     * @return array
+     *
+     * @author Etienne de Longeaux <etienne_delongeaux@hotmail.com>
+     */
+    public function defaultMenu($options = null)
+    {
+        if ( !isset($options['template']) || empty($options['template']) ) {
+            throw ExtensionException::optionValueNotSpecified('template', __CLASS__);
+        } else {
+            $template    = $options['template'];
+        }
+    	$response     = $this->container->get('templating')->renderResponse("PiAppTemplateBundle:Template\\Widgetimport:$template", $options);
+    	
+    	return $response->getContent();
+    }    
+    
+    /**
+     * Default render
+     *
+     * @param array        $result
+     * @param array        $options
+     * @access private
+     * @return string
+     *
+     * @author (c) Etienne de Longeaux <etienne_delongeaux@hotmail.com>
+     * @author (c) Pedro Felix <contactpfelix@gmail.com>
+     */
     private function widgetimportAction($result, $options = null)
     {
-        if ( isset($options['locale']) )
+        // set Options
+        if ( isset($options['locale']) ) {
             $this->locale    = $options['locale'];
-        
-        // Options management
-        if ( !isset($options['template']) || empty($options['template']) )
-            $template   = "searchlucene-result.html.twig";
-        else
+        }
+        if ( !isset($options['template']) || empty($options['template']) ) {
+            throw ExtensionException::optionValueNotSpecified('template', __CLASS__);
+        } else {
             $template    = $options['template'];
-        
+        }
         // a = floor(a/b)*b + a%b;
         $nb    = count($result);
         $q    = floor($nb/3);
@@ -244,8 +483,9 @@ class PiwidgetimportManager extends PiJqueryExtension
                                 }).prependTo($(settings.handleSelector,this));
                             }
                         });
+                        
                     },
-                    
+
                     makeSortable : function () {
                         var PIimportwidget = this,
                             $ = this.jQuery,
@@ -257,7 +497,7 @@ class PiwidgetimportManager extends PiJqueryExtension
                                         if (!this.id) {
                                             this.id = 'widget-no-id-' + i;
                                         }
-                                        notSortable += '#' + this.id + ',';
+                                        //notSortable += '#' + this.id + ',';
                                     }
                                 });
                                 return $('> li:not(' + notSortable + ')', settings.columns);
@@ -313,20 +553,24 @@ class PiwidgetimportManager extends PiJqueryExtension
         // We close the buffer.
         ob_end_flush ();
         
-        //print_r($archive);exit;
-        return    $response->getContent() . $_content;        
+        if ( !isset($options['noJs']) || !($options['noJs']) ) {
+            return    $response->getContent() . $_content;        
+        } else {
+            return    $response->getContent();
+        }
     }
     
     /**
-     * Define semantique tree html FORMULAIRE.
+     * Generate all form builders.
      *
      * <code>
      *        {% set options_widgetimport = {
      *                'action':'widgetimport',
      *                'menu': 'widgetimport',
      *                'template': 'default.html.twig',
-     *                'locale': 'fr',
-     *                }
+     *                'locale': app.request.locale,
+     *                'noJs' : false,
+     *             }
      *        %}
      *        {{ renderJquery('TOOL', 'widgetimport', options_widgetimport )|raw }}
      * </code>
